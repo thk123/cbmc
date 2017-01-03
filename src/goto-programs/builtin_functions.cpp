7495a325d8fe2311426a3a6b91abf74c805fc0ff
Michael Tautschnig <michael.tautschnig@qmul.ac.uk>
2017-01-03 14:53:46 +0000

    Generalize ID_malloc to ID_allocate with optional zero-init
    __CPROVER_allocate takes two arguments, where the second requests
    zero-initialization of the newly allocated object. Thus `calloc` can be
    implemented efficiently.

--- src/goto-programs/builtin_functions.cpp
+++ src/goto-programs/builtin_functions.cpp
@@ -600,8 +600,10 @@ void goto_convertt::do_java_new(
   }
 
   // we produce a malloc side-effect, which stays
-  side_effect_exprt malloc_expr(ID_malloc);
+  side_effect_exprt malloc_expr(ID_allocate);
   malloc_expr.copy_to_operands(object_size);
+  // could use true and git rid of the code below
+  malloc_expr.copy_to_operands(false_exprt());
   malloc_expr.type()=rhs.type();
 
   goto_programt::targett t_n=dest.add_instruction(ASSIGN);
@@ -656,8 +658,10 @@ void goto_convertt::do_java_new_array(
   }
 
   // we produce a malloc side-effect, which stays
-  side_effect_exprt malloc_expr(ID_malloc);
+  side_effect_exprt malloc_expr(ID_allocate);
   malloc_expr.copy_to_operands(object_size);
+  // code use true and get rid of the code below
+  malloc_expr.copy_to_operands(false_exprt());
   malloc_expr.type()=rhs.type();
 
   goto_programt::targett t_n=dest.add_instruction(ASSIGN);
