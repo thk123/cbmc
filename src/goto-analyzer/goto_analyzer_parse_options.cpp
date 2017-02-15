08ef2a0f04a96cb316a6997a55a74e453b4dbeae
Daniel Poetzl <daniel.poetzl@cs.ox.ac.uk>
2017-02-15 17:09:53 +0000

    more status output

--- src/goto-analyzer/goto_analyzer_parse_options.cpp
+++ src/goto-analyzer/goto_analyzer_parse_options.cpp
@@ -337,7 +337,9 @@ int goto_analyzer_parse_optionst::doit()
     }
 
     if(process_goto_program(options))
-    return 6;
+      return 6;
+
+    status() << "Starting analysis" << eom;
 
     if(cmdline.isset("taint"))
     {
