f1497e1141046d48b700273eba1b508b7054e79c
Peter Schrammel <peter.schrammel@diffblue.com>
2017-07-02 22:49:32 +0100

    Generalise run_lint to take a command to run on a diff

---
+++ scripts/run_diff.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+set -e
+
+script_folder=`dirname $0`
+absolute_repository_root=`git rev-parse --show-toplevel`
+check_script=$1
+
+if [[ "$#" -gt 3 ]]
+then
+    echo "Script for running the CPP linter only on modified lines. Arguments:"
+    echo "target - a git reference to the branch we want to compare against (default: 'master')"
+    echo "tip - a git reference to the commit with changes (default: current working tree)"
+    exit 1
+fi
+
+if ! [[ -e $script_folder/filter_by_diff.py ]]
+then
+  echo "Filter script could not be found in the $script_folder directory"
+  echo "Ensure filter_by_diff.py is inside the $script_folder directory then run again"
+  exit 1
+fi
+
+if [[ "$#" -gt 1 ]]
+then
+    git_start=$2
+else
+    git_start="master"
+fi
+
+if [[ "$#" -gt 2 ]]
+then
+    git_end=$3
+    git_merge_base_end=$3
+else
+    git_end=""
+    git_merge_base_end="HEAD"
+fi
+
+git_start=`git merge-base $git_start $git_merge_base_end`
+
+cleanup()
+{
+  rm -f $diff_file
+}
+
+trap cleanup EXIT
+
+diff_file=`mktemp`
+
+git diff $git_start $git_end > $diff_file
+
+# Get the list of files that have changed
+diff_files=`git diff --name-only $git_start $git_end`
+
+for file in $diff_files; do
+  file=$absolute_repository_root/$file
+  # If the file has been deleted we don't want to run the linter on it
+  if ! [[ -e $file ]]
+  then
+    continue
+  fi
+
+  # Run the linting script and filter:
+  # The errors from the linter go to STDERR so must be redirected to STDOUT
+  result=`eval $check_script | $script_folder/filter_by_diff.py $file $diff_file $absolute_repository_root`
+
+  # Providing some errors were relevant we print them out
+  if [ "$result" ]
+  then
+    are_errors=1
+    (>&2 echo "$result")
+  fi
+done
+
+# Return an error code if errors are found
+exit $are_errors
--- scripts/travis_lint.sh
+++ scripts/travis_lint.sh
@@ -5,9 +5,15 @@ set -e
 script_folder=`dirname $0`
 pip install --user unidiff
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  $script_folder/run_lint.sh HEAD~1 # Check for errors introduced in last commit
-else
-  $script_folder/run_lint.sh $TRAVIS_BRANCH # Check for errors compared to merge target
+if ! [[ -e $script_folder/cpplint.py ]]
+then
+  echo "CPP Linter script could not be found in the $script_folder directory"
+  echo "Ensure cpplint.py is inside the $script_folder directory then run again"
+  exit 1
 fi
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  $script_folder/run_diff.sh '$script_folder/cpplint.py $file 2>&1 >/dev/null' HEAD~1 # Check for errors introduced in last commit
+else
+  $script_folder/run_diff.sh '$script_folder/cpplint.py $file 2>&1 >/dev/null' $TRAVIS_BRANCH # Check for errors compared to merge target
+fi
