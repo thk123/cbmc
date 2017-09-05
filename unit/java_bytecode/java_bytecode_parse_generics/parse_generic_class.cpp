0f1b8cfa2d96d392dd47919b87668157264105c5
Matthias Güdemann <matthias.guedemann@diffblue.com>
2017-09-05 17:27:53 +0200

    Add unit test for java_generic_type

--- unit/java_bytecode/java_bytecode_parse_generics/parse_generic_class.cpp
+++ unit/java_bytecode/java_bytecode_parse_generics/parse_generic_class.cpp
@@ -7,7 +7,7 @@
 \*******************************************************************/
 
 #include <catch.hpp>
-
+#include <iostream>
 #include <istream>
 #include <memory>
 
@@ -30,7 +30,7 @@ SCENARIO("java_bytecode_parse_generics",
     "./java_bytecode/java_bytecode_parse_generics");
 
   // Configure the language
-  null_message_handlert message_handler;
+  stream_message_handlert message_handler(std::cout);
   java_lang->get_language_options(command_line);
   java_lang->set_message_handler(message_handler);
 
@@ -61,6 +61,42 @@ SCENARIO("java_bytecode_parse_generics",
         REQUIRE(is_java_generics_class_type(java_class_type));
         java_generics_class_typet java_generics_class_type=
           to_java_generics_class_type(java_class_type);
+      }
+    }
+  }
+  GIVEN("Some class files with generic type variable")
+  {
+    WHEN("Parsing a class with type variable")
+    {
+      java_lang->parse(java_code_stream, "generics$element.class");
+
+      symbol_tablet new_symbol_table;
+      java_lang->typecheck(new_symbol_table, "");
+
+      java_lang->final(new_symbol_table);
+
+      REQUIRE(new_symbol_table.has_symbol("java::generics$element"));
+      THEN("The symbol type should be generic")
+      {
+        const symbolt &class_symbol=new_symbol_table.lookup("java::generics$element");
+        const typet &symbol_type=class_symbol.type;
+
+        REQUIRE(symbol_type.id()==ID_struct);
+        class_typet class_type=to_class_type(symbol_type);
+        REQUIRE(class_type.is_class());
+        java_class_typet java_class_type=to_java_class_type(class_type);
+        REQUIRE(is_java_generics_class_type(java_class_type));
+        java_generics_class_typet java_generics_class_type=
+          to_java_generics_class_type(java_class_type);
+        REQUIRE(java_generics_class_type.subtypes().size()==1);
+        typet &type_var=java_generics_class_type.subtypes().front();
+        REQUIRE(is_java_generic_type(type_var));
+        java_generic_typet generic_type_var=to_java_generic_type(type_var);
+        REQUIRE(generic_type_var.id()=="java::generics$element::E");
+        typet &sub_type=generic_type_var.subtype();
+        REQUIRE(sub_type.id()==ID_symbol);
+        symbol_typet &bound_type=to_symbol_type(sub_type);
+        REQUIRE(bound_type.get_identifier()=="java::java.lang.Object");
       }
     }
   }
