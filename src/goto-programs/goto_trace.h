bf19ecef9afb7a2c53c9ccb7cbc7a5f8d7e98832
thk123 <thomas@diffblue.com>
2019-02-05 10:58:41 +0000

    Add basic docs to goto trace and goto trace step
    Converting some existing comments to doxygen formatting and adding some
    basic information.

--- src/goto-programs/goto_trace.h
+++ src/goto-programs/goto_trace.h
@@ -24,12 +24,12 @@ Date: July 2005
 
 #include <goto-programs/goto_program.h>
 
-/*! \brief TO_BE_DOCUMENTED
- * \ingroup gr_goto_symex
-*/
+/// A single step of a \ref goto_tracet
+/// \ingroup gr_goto_symex
 class goto_trace_stept
 {
 public:
+  /// The index of the step within the trace, the first step is index 0??
   std::size_t step_nr;
 
   bool is_assignment() const      { return type==typet::ASSIGNMENT; }
@@ -50,6 +50,7 @@ public:
   bool is_memory_barrier() const  { return type==typet::MEMORY_BARRIER; }
   bool is_atomic_begin() const    { return type==typet::ATOMIC_BEGIN; }
   bool is_atomic_end() const      { return type==typet::ATOMIC_END; }
+
 
   enum class typet
   {
@@ -74,57 +75,80 @@ public:
     ATOMIC_END
   };
 
+  /// The kind of statement that this step relates to
   typet type;
 
-  // we may choose to hide a step
+  /// we may choose to hide a step
+  /// TODO: expand to what steps are hidden
   bool hidden;
 
-  // this is related to an internal expression
+  /// this is related to an internal expression
+  /// TODO: expand to what steps are internal
   bool internal;
 
-  // we categorize
+  /// we categorize
+  /// TODO: Literally what?
   enum class assignment_typet { STATE, ACTUAL_PARAMETER };
   assignment_typet assignment_type;
 
-  // The instruction that created this step
-  // (function calls are in the caller, function returns are in the callee)
+  /// The function that created this step
+  /// For FUNCTION_CALL steps, the function_id is the caller
+  /// For FUNCTION_RETURN steps the function_id is the callee
+  /// For example:
+  /// 0: foo(); // function_id = CPROVER_START
+  /// 1: return; // function_id = foo
+  /// 2: return // function_id = CPROVER_START
   irep_idt function_id;
+
+  /// A iterator into the GOTO statement that this trace step corresponds to
   goto_programt::const_targett pc;
 
-  // this transition done by given thread number
+  /// this transition done by given thread number
   unsigned thread_nr;
 
-  // for assume, assert, goto
+  /// For \ref typet::ASSUME, \ref typet::ASSERT, \ref typet::GOTO this
+  /// contains the evaluated condition value
   bool cond_value;
+  /// See \ref cond_value
   exprt cond_expr;
 
-  // for assert
+  /// For \ref typet::ASSERT steps, this is the id of the associated goal
+  /// TODO: identify appropriate reference
   irep_idt property_id;
+  /// For \ref typet::ASSERT steps, this is the comment of the associated goal
+  /// TODO: identify appropriate reference
   std::string comment;
 
-  // the full, original lhs expression, after dereferencing
+  /// The full, original lhs expression, after dereferencing
+  /// TODO: are these only applicable in ASSIGNMENT
   exprt full_lhs;
 
-  // the object being assigned
+  /// The l-value object being assigned (i.e. after walking up member and index access
+  /// and skipping type casts).
+  /// TODO: are these only applicable in ASSIGNMENT
   optionalt<symbol_exprt> get_lhs_object() const;
 
-  // A constant with the new value of the lhs
+  /// A constant with the new value of the lhs, that is, the rhs.
+  /// TODO: are these only applicable in ASSIGNMENT
   exprt full_lhs_value;
 
-  // for INPUT/OUTPUT
+  /// for INPUT/OUTPUT
+  /// TODO: find out what these strings actually are
   irep_idt format_string, io_id;
   typedef std::list<exprt> io_argst;
   io_argst io_args;
   bool formatted;
 
-  // for function calls
+  /// For \ref typet::FUNCTION_CALL stores the function that has been called
+  /// (in the example in \ref function_id), step 0, this would have the value foo)
   irep_idt called_function;
 
-  // for function call
+  /// For \ref typet::FUNCTION_CALL, stores the evaluated?? values for the function
+  /// arguments
   std::vector<exprt> function_arguments;
 
-  /*! \brief outputs the trace step in ASCII to a given stream
-  */
+  /// Outputs the trace step in ASCII to a given stream
+  /// TODO: document what this looks like
   void output(
     const class namespacet &ns,
     std::ostream &out) const;
@@ -145,25 +169,28 @@ public:
   }
 };
 
-/*! \brief TO_BE_DOCUMENTED
- * \ingroup gr_goto_symex
-*/
+/// Represents a trace that has come out of the solver
+/// TODO: is this the same class that is representing a trace
+/// before and after SSA conversion?
 class goto_tracet
 {
 public:
   typedef std::list<goto_trace_stept> stepst;
   stepst steps;
 
+  /// The language of the trace, TODO: find appropriate reference
   irep_idt mode;
 
+  /// TODO: find the purpose of this method
   void clear()
   {
     mode.clear();
     steps.clear();
   }
 
-  /*! \brief outputs the trace in ASCII to a given stream
-  */
+  /// Outputs the trace in ASCII to a given stream
+  /// TODO: document the format
+  /// See \ref goto_trace_stept::output
   void output(
     const class namespacet &ns,
     std::ostream &out) const;
@@ -174,13 +201,14 @@ public:
     other.mode.swap(mode);
   }
 
+  /// Append a step to the trace.
   void add_step(const goto_trace_stept &step)
   {
     steps.push_back(step);
   }
 
-  // retrieves the final step in the trace for manipulation
-  // (used to fill a trace from code, hence non-const)
+  /// Retrieves the final step in the trace for manipulation
+  /// (used to fill a trace from code, hence non-const)
   goto_trace_stept &get_last_step()
   {
     return steps.back();
@@ -189,7 +217,7 @@ public:
   /// Returns the property IDs of all assertions in the trace
   std::vector<irep_idt> get_all_property_ids() const;
 
-  // delete all steps after (not including) s
+  /// delete all steps after (not including) s
   void trim_after(stepst::iterator s)
   {
     PRECONDITION(s != steps.end());
@@ -197,18 +225,33 @@ public:
   }
 };
 
+
+/// Configuration options for use in outputing the trace
+/// See \ref show_goto_trace
 struct trace_optionst
 {
+  /// Should the \ref goto_trace_stept::full_lhs include a full
+  /// structured irep dump of the lhs.
+  /// TODO: find out if this is only for json
   bool json_full_lhs;
+  /// TODO: find out what this controls
   bool hex_representation;
+  /// TODO: find out what this controls
   bool base_prefix;
+  /// TODO: find out what this controls
   bool show_function_calls;
+  /// TODO: find out what this controls
   bool show_code;
+  /// TODO: find out what this controls
   bool compact_trace;
+  /// TODO: find out what this controls
   bool stack_trace;
 
+  /// The default trace printing options. See \ref trace_optionst::trace_optionst()
+  /// for what these values are.
   static const trace_optionst default_options;
 
+  /// Configure the trace printing options from the command line.
   explicit trace_optionst(const optionst &options)
   {
     json_full_lhs = options.get_bool_option("trace-json-extended");
@@ -233,17 +276,21 @@ private:
   };
 };
 
+// NOLINTNEXTLINE
+/// See \ref show_goto_trace(messaget::mstreamt &, const namespacet &, const goto_tracet &, const trace_optionst &
 void show_goto_trace(
   messaget::mstreamt &out,
   const namespacet &,
   const goto_tracet &);
 
+/// Output the goto trace to the provided stream
 void show_goto_trace(
   messaget::mstreamt &out,
   const namespacet &,
   const goto_tracet &,
   const trace_optionst &);
 
+/// TODO: figure out what this does
 void trace_value(
   messaget::mstreamt &out,
   const namespacet &,
